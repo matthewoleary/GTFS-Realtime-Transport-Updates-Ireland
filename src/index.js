1edf9c160845c1d340c500f67be8a2bc2b0ea9dc
Matthew O'Leary <>
2026-02-17 11:50:34 +0100

    Adds realtime vehicle positions plugin and updates existing server plugins.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-
-const server = require('./server');
-const config = require('./config');
+import { CronJob } from 'cron';
+import app from './app.js';
+import config from './config.js';
+import { runImport } from './database/import/gtfs-import.js';
+import Logger from './logger.js';
+const logger = new Logger({ client: 'APP' });
 
 const startServer = async env => {
 	try {
 		let configEnv;
 		env === 'prod' ? configEnv = config.prod : configEnv = config.test;
-		const app = await server(configEnv);
-		await app.start();
-		console.log(`Server running at http://${configEnv.host}:${configEnv.port}`);
+		const appInstance = await app(configEnv);
+
+		await executeRunImport();
+
+		// Every hour will check for GTFS static schedule updates and import if available.
+		logger.info('Scheduling GTFS static schedule updates every hour');
+		const job = new CronJob('0 * * * *', async () => {
+			try {
+				logger.info('Running scheduled GTFS static schedule update check and import if available. Time: ' + new Date().toISOString());
+				await executeRunImport();
+			} catch (error) {
+				logger.error('Scheduled GTFS import failed: ' + error);
+			}
+		}, null, true);
+
+		await appInstance.start();
+		logger.success(`Server running at http://${configEnv.host}:${configEnv.port}`);
 	} catch (error) {
-		console.log('Startup error', error);
+		logger.error('Startup error: ' + error);
 	}
 };
 
-startServer(process.argv[2]);
+const executeRunImport = async () => {
+	try {
+		await runImport();
+		logger.success('Import completed successfully');
+	} catch (error) {
+		logger.error('Import failed: ' + error.message);
+	}
+}
+
+startServer(process.argv[2]).catch(error => {
+	logger.error('Failed to start server: ' + error);
+	process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+	logger.error('Unhandled Rejection: ' + err);
+});
+
+process.on('uncaughtException', (err) => {
+	logger.error('Uncaught Exception: ' + err);
+});
