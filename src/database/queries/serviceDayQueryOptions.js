28a3e339b71793bd115f27e4c8761390d1a73322
Matthew O'Leary <>
2026-02-27 20:10:10 +0100

    Add JSDoc comments for ServiceDayQueryOptions object

--- src/database/queries/serviceDayQueryOptions.js
+++ src/database/queries/serviceDayQueryOptions.js
@@ -1,11 +1,34 @@
-// ServiceDay.js
-// Represents a service day for GTFS queries (e.g., for night services)
-
+/**
+  * Represents options for a GTFS service day query (e.g., for night services).
+*/
 class ServiceDayQueryOptions {
+  /**
+   * @param {Object} options
+   * @param {string} options.dayColumn - The name of the day column (e.g., 'monday').
+   * @param {string} options.date - The service date in YYYYMMDD format.
+   * @param {number} options.upperBoundTimestamp - The upper bound timestamp (in seconds).
+   * @param {number} options.lowerBoundTimestamp - The lower bound timestamp (in seconds).
+   */
   constructor({ dayColumn, date, upperBoundTimestamp, lowerBoundTimestamp }) {
+    /**
+     * The name of the day column (e.g., 'monday').
+     * @type {string}
+     */
     this.dayColumn = dayColumn;
+    /**
+     * The service date in YYYYMMDD format.
+     * @type {string}
+     */
     this.date = date;
+    /**
+     * The upper bound timestamp (in seconds).
+     * @type {number}
+     */
     this.upperBoundTimestamp = upperBoundTimestamp;
+    /**
+     * The lower bound timestamp (in seconds).
+     * @type {number}
+     */
     this.lowerBoundTimestamp = lowerBoundTimestamp;
   }
 }
