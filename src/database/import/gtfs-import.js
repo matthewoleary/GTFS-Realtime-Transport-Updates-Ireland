ca94575727ca9973396b017b23801f8b21a0ea50
Matthew O'Leary <>
2026-03-13 18:35:29 +0100

    PR Feedback: Removing process exit in error handling as it will terminate the server process on any import error

--- src/database/import/gtfs-import.js
+++ src/database/import/gtfs-import.js
@@ -1,20 +1,9 @@
 import ImportLogger from './importLogger.js';
 import { gtfsImportConfig } from '../config.js';
 
-function handleError(err) {
-    const text = err || 'Unknown Error';
-    console.error(`\n${text}\n`);
-    console.error(err);
-    process.exit(1);
-}
-
-async function setupImport(dbClientInstance) {
+export async function runImport(dbClientInstance) {
     const logger = new ImportLogger({ client: 'MYSQL_IMPORTER' });
     const MysqlImporter = (await import('./mysql-import.js')).default;
     const importer = new MysqlImporter(gtfsImportConfig, logger, dbClientInstance);
     await importer.import();
-}
-
-export async function runImport(dbClientInstance) {
-    await setupImport(dbClientInstance).catch(handleError);
 }
