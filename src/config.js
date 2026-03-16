59985d11c6524b0fbc17eed032e44fb62535307a
Matthew O'Leary <>
2026-03-16 22:20:35 +0100

    Add integration tests which run against testing docker container

--- src/config.js
+++ src/config.js
@@ -13,14 +13,19 @@ const {
 	DOCKER_SQL_PASSWORD,
 	DOCKER_SQL_HOST,
 	DOCKER_SQL_DATABASE,
+	DOCKER_SQL_PORT,
+	TEST_PORT,
+	TEST_DOCKER_SQL_USER,
+	TEST_DOCKER_SQL_PASSWORD,
+	TEST_DOCKER_SQL_HOST,
+	TEST_DOCKER_SQL_DATABASE,
+	TEST_DOCKER_SQL_PORT,
 	GTFSR_API_KEY,
 	GTFSR_API_TRIP_UPDATES_URL,
 	GTFSR_API_TRIP_UPDATES_URL_FALLBACK,
 	GTFSR_API_VEHICLE_POSITIONS_URL,
 	GTFSR_API_VEHICLE_POSITIONS_URL_FALLBACK
 } = process.env;
-
-const sqlEncrypt = process.env.SQL_ENCRYPT === 'true';
 
 assert(PORT, 'PORT is required');
 assert(HOST, 'HOST is required');
@@ -29,6 +34,7 @@ assert(DOCKER_SQL_HOST, 'DOCKER_SQL_HOST is required');
 assert(DOCKER_SQL_USER, 'DOCKER_SQL_USER is required');
 assert(DOCKER_SQL_PASSWORD, 'DOCKER_SQL_PASSWORD is required');
 assert(DOCKER_SQL_DATABASE, 'DOCKER_SQL_DATABASE is required');
+assert(DOCKER_SQL_PORT, 'DOCKER_SQL_PORT is required');
 assert(GTFSR_API_KEY, 'GTFSR_API_KEY is required');
 assert(GTFSR_API_TRIP_UPDATES_URL, 'GTFSR_API_TRIP_UPDATES_URL is required');
 assert(GTFSR_API_VEHICLE_POSITIONS_URL, 'GTFSR_API_VEHICLE_POSITIONS_URL is required');
@@ -49,15 +55,31 @@ const prod = {
 		database: DOCKER_SQL_DATABASE,
 		user: DOCKER_SQL_USER,
 		password: DOCKER_SQL_PASSWORD,
-		options: {
-			encrypt: sqlEncrypt,
-			enableArithAbort: true,
-			validateBulkLoadParameters: true,
-			trustServerCertificate: true
-		}
+		port: DOCKER_SQL_PORT
+	}
+};
+
+const test = {
+	port: TEST_PORT,
+	host: HOST,
+	url: HOST_URL,
+	gtfsr: {
+		apiKey: GTFSR_API_KEY,
+		apiTripUpdatesUrl: GTFSR_API_TRIP_UPDATES_URL,
+		apiTripUpdatesUrlFallback: GTFSR_API_TRIP_UPDATES_URL_FALLBACK,
+		apiVehiclePositionsUrl: GTFSR_API_VEHICLE_POSITIONS_URL,
+		apiVehiclePositionsUrlFallback: GTFSR_API_VEHICLE_POSITIONS_URL_FALLBACK
+	},
+	sql: {
+		host: TEST_DOCKER_SQL_HOST,
+		database: TEST_DOCKER_SQL_DATABASE,
+		user: TEST_DOCKER_SQL_USER,
+		password: TEST_DOCKER_SQL_PASSWORD,
+		port: TEST_DOCKER_SQL_PORT
 	}
 };
 
 export default {
-	prod
+	prod,
+	test
 };
