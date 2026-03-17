461d435ca9482ebedee4d3f932155431ceba0a83
Matthew O'Leary <>
2026-03-17 13:48:14 +0100

    Rename getTripsAtStop route registration and functions

--- src/server/api/test/routes.test.js
+++ src/server/api/test/routes.test.js
@@ -16,15 +16,15 @@ describe('registerRoutes', () => {
     const getStops = (await import('../routes/getStops.js')).default;
     const getRoutes = (await import('../routes/getRoutes.js')).default;
     const getAgencies = (await import('../routes/getAgencies.js')).default;
-    const getStopTimesForTripRoute = (await import('../routes/getStopTimesForTrip.js')).default;
     const getTripsRoute = (await import('../routes/getTrips.js')).default;
+    const getStopTimesForTrip = (await import('../routes/getStopTimesForTrip.js')).default;
     const getShapesRoute = (await import('../routes/getShapes.js')).default;
     const getTripsAtStopRoute = (await import('../routes/getTripsAtStop.js')).default;
     expect(getStops).toHaveBeenCalledWith(server);
     expect(getRoutes).toHaveBeenCalledWith(server);
     expect(getAgencies).toHaveBeenCalledWith(server);
-    expect(getStopTimesForTripRoute).toHaveBeenCalledWith(server);
     expect(getTripsRoute).toHaveBeenCalledWith(server);
+    expect(getStopTimesForTrip).toHaveBeenCalledWith(server);
     expect(getShapesRoute).toHaveBeenCalledWith(server);
     expect(getTripsAtStopRoute).toHaveBeenCalledWith(server);
   });
