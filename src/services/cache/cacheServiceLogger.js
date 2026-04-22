6435992f9dbcee07282a58d3c2298fea771091c9
Matthew O'Leary <>
2026-04-22 17:33:39 +0200

    Add cacheService, logger and unit tests

---
+++ src/services/cache/cacheServiceLogger.js
@@ -0,0 +1,24 @@
+import Logger from '../../../src/logger.js';
+
+/**
+ * CacheServiceLogger class for logging cache service events.
+ * Extends the shared Logger class and adds a query() method for cache operations.
+ */
+export default class CacheServiceLogger extends Logger {
+    constructor(options = {}) {
+        super({
+            client: options.client || 'CACHESERVICELOGGER',
+            successMessage: options.successMessage || 'Cache service operation successful.',
+            errorMessage: options.errorMessage || 'Cache service error encountered.'
+        });
+        this.queryMessage = options.queryMessage || 'Executing cache service operation.';
+    }
+
+    /**
+     * Logs a cache service operation event.
+     * @param {string} [message] - The message to log.
+     */
+    query(message) {
+        this.info(`[QUERY] ${message || this.queryMessage}`);
+    }
+}
