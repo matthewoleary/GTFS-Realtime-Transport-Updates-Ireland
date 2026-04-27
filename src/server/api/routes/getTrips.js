067ef8122c8c0dd18f801369e052ee9a928c0d58
Matthew O'Leary <>
2026-04-27 14:50:16 +0200

    PR Feedback: Remove no longer needed imports

--- src/server/api/routes/getTrips.js
+++ src/server/api/routes/getTrips.js
@@ -2,7 +2,6 @@ import ServerLogger from '../../serverLogger.js';
 import { extractIdsFromParam } from './utils.js';
 import { getDatabaseClient, getRealtimeVehiclePositionsClient, getCacheService } from '../index.js';
 import { getUnixTimestamp } from '../../../utils/timestampUtils.js';
-import { CacheService } from '../../../services/cache/cacheService.js';
 
 /**
  * Registers the /api/trips GET route for fetching GTFS trip data.
